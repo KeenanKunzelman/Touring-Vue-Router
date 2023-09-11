e910656e767ebc8800bbdee2af614d75a528eece
Keenan Kunzelman <keenan@cyberxelite.com>
2023-09-11 10:48:49 -0400

    finished lesson

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,18 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register () {
+      //call api 
+      this.$router.push({
+        name: 'EventDetails',
+      })
+    }
+  }
 }
 </script>
