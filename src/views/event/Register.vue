7f38121d5d0a36b0a00b7d3f399683f3fde70f2f
Keenan Kunzelman <keenan@cyberxelite.com>
2023-09-12 17:56:37 -0400

    finished lesson 7

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -6,11 +6,16 @@
 <script>
 export default {
   props: ['event'],
+  inject: ['Gstore'],
   methods: {
     register() {
       // Call to API
       // If registered then redirect to event details
-
+      this.Gstore.flashMessage = 
+        'You are successfully registered for ' + this.event.title
+      setTimeout(() => {
+        this.Gstore.flashMessage = ''
+      }, 3000);
       this.$router.push({
         name: 'EventDetails'
       })
